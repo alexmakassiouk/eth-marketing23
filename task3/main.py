ca53e4dc60b24271c841791615c7b60590863827
ComputasAlex <almax@computas.com>
2023-11-23 14:10:48 +0100

    Merge Ida to task 3 Update main python file

--- task3/main.py
+++ task3/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pandas import DataFrame
+from IPython.display import display
 
 # Import data from data.csv with pandas
 def get_raw_data() -> DataFrame:
@@ -8,10 +9,10 @@ def get_raw_data() -> DataFrame:
 
 # Explore data from get_data()
 def explore_data(data: DataFrame) -> None:
-    print(data.head())
-    print(data.describe())
-    print(data.info())
-    print(data.columns)
+    display(data.head())
+    display(data.describe())
+    display(data.info())
+    display(data.columns)
 
 # Fill all NaN values with 0
 def clean_data(data: DataFrame) -> DataFrame:
@@ -19,7 +20,7 @@ def clean_data(data: DataFrame) -> DataFrame:
     return data
 
 def modify_month_values(data: DataFrame) -> None:
-    data['time_month '] = data['time_month '].apply(lambda x: x - 1)
+    data['time_month '] = (data['time_month '] + 9)%12
 # Replace all values in time_month with the number that is 1 less than the value, according to CPA instructions
 
 def get_data() -> DataFrame:
@@ -40,10 +41,14 @@ def get_max_time_df(data: DataFrame) -> DataFrame:
     max_time_df = get_max_column_df(max_year_df, 'time_month ')
     return max_time_df
 
-# Method that returns a list containing the number of rows for each value in the column 'cohort'
 def get_cohort_sizes(data: DataFrame) -> list[int]:
-    cohort_sizes = data.groupby('cohort ').size()
-    return cohort_sizes.values.tolist()
+    # Return the number of unique user values for each cohort
+    cohort_sizes = data.groupby('cohort ')["user  "].nunique()
+    return cohort_sizes
+
+def get_customer_base_sizes(data: DataFrame) -> list[int]:
+    customer_base_sizes = data.groupby('time_month ')["user  "].nunique()
+    return customer_base_sizes
 
 # TASK 3
 
